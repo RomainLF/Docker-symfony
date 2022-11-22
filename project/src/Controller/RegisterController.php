c97781cf01746f6190be4666770498682829b4ef
romainLF <romain.lefloch@hotmail.com>
2022-11-22 20:50:58 +0000

    password encoded

--- project/src/Controller/RegisterController.php
+++ project/src/Controller/RegisterController.php
@@ -1,18 +1,50 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class RegisterController extends AbstractController
 {
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine) {
+        $this->doctrine = $doctrine;
+    }
+
     /**
-     * @Route("/inscription", name="app_register")
+     * @Route("/inscription", name="register")
      */
-    public function index(): Response
+
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('register/index.html.twig');
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+
+            $user -> setPassword($password);
+
+            $em = $this->doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
